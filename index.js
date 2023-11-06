e81581670d9937de9d35d8a542bd6d0f66900f0d
Zisan05 <mdzisanislam69@gmail.com>
2023-11-07 02:54:19 +0800

    new db create

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const cors = require('cors');
-require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app =express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["http://localhost:5173"],
+    credentials:true
+  }
+));
 app.use(express.json());
+app.use(cookieParser());
+
+// JWT middleware
+
+const verify = async(req,res,next) => {
+  const token = req.cookies?.token;
+  if(!token){
+   return res.status(401).send({status: "unauthorize",code: "401"});
+  } 
+  jwt.verify(token,process.env.SECRET,(err,decoded) => {
+    if(err){
+      return res.status(401).send({status: "unauthorize",code: "401"});
+    }
+      console.log("value in the token",decoded)
+      req.user = decoded;
+      next();
+  })
+  
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a3cooza.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,10 +51,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Create Assignment
+    // Create Assignment DB
 
     const AssCollection = client.db("GroupStudyDB").collection('Assignment');
+
+    // Submitted Assignment DB
+
     const subCollection = client.db("GroupStudyDB").collection('submitAssignment');
+
+
+    // JWT
+
+app.post("/jwt",async(req,res) => {
+  const body = req.body;
+  const token = jwt.sign(body, process.env.SECRET,{expiresIn : '10h'});
+   
+  res.cookie('token',token,{
+    httpOnly: true,
+    secure: false,
+    sameSite: 'none'
+  }).send({msg: 'success'})
+   
+
+})
+
+    //  My Assignment DB
+
+    const MyCollection = client.db("GroupStudyDB").collection('MyAssignment');
 
     // Create Assignment DB
     app.post('/assignment',async(req,res) => {
@@ -44,7 +93,7 @@ async function run() {
       res.send(result);
     })
 
-    app.delete('/assignment/:id',async(req,res) => {
+    app.delete('/assignment/:id', async(req,res) => {
          const id = req.params.id;
          const quary = {_id: new ObjectId(id)};
          const result = await AssCollection.deleteOne(quary);
@@ -88,6 +137,46 @@ async function run() {
 
 app.get('/submit',async(req,res) => {
   const cursor = subCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
+
+app.get('/submit/:id', async(req,res) => {
+  const id = req.params.id;
+     const quary = {_id: new ObjectId(id)};
+     const result = await subCollection.findOne(quary);
+  res.send(result);
+})
+
+app.put('/submit/:id',async(req,res) => {
+  const id = req.params.id;
+  const filter = {_id : new ObjectId(id)};
+  const updated = req.body;
+    const markUpdate = {
+      $set: {
+        marks: updated.marks,
+        status: updated.status,
+        feedback: updated.feedback,
+        pdf: updated.pdf,
+        text: updated.text
+      }
+    } 
+    const result = await subCollection.updateOne(filter,markUpdate);
+    res.send(result);
+   
+ })
+
+  //  My Assignment DB
+
+  app.post('/my',async(req,res) => {
+    const newSubmit = req.body;
+    console.log(newSubmit);
+    const result = await MyCollection.insertOne(newSubmit);
+    res.send(result);
+})
+
+app.get('/my',async(req,res) => {
+  const cursor = MyCollection.find();
   const result = await cursor.toArray();
   res.send(result);
 })
